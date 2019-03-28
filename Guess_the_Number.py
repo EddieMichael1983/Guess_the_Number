184c46df9f49c8784ab7858bc83e2bdf543e744d
EddieMichael1983 <46540863+EddieMichael1983@users.noreply.github.com>
2019-03-27 19:13:27 -0700

    Update Guess_the_Number.py

--- Guess_the_Number.py
+++ Guess_the_Number.py
@@ -3,34 +3,28 @@
 import random
 num1 = random.randint(1,10)
 
-counter = 1    #sets up counting function (starting at 1 because the user will guess at least one time)
+counter = 1  #counter set at 1 because user will guess at least one time
 
 while True:
 
     user_guess = input("Guess what number between 1 and 10 I'm thinking of?: ")
     user_guess = int(user_guess)
 
-    if user_guess == num1:        #line 13 and 14 stop the program once the user has guessed correctly
-        break
-    counter = counter + 1         #line 15 continues to add to the count for each time the user guesses incorrectly
-
-    if user_guess == num1:
+    if user_guess == num1:                #Tells the computer to print 'You got it!' when the user guesses correctly
         print("You got it!")
+        break
+    counter = counter + 1
 
-    elif user_guess > num1:
+    if user_guess > num1:
         print("Too high!")
 
     elif user_guess < num1:
         print("Too low!")
 
-print(f"You guessed {counter} times")    #function showing the user how many times they guessed until getting the right number
+    if counter > 5 and user_guess != num1:   #allows the user 5 maximum guesses
+        print("You lost. Game Over.")
+        break
+print(f"You guessed {counter} times.")
 
 
 
-#Additional Challenges to add later
-
-#Using a while loop, allow the user to guess 10 times. If they fail to guess the number after 10 tries, the user is told they've lost.
-
-#Tell the user whether their current guess is closer than their last.
-
-#Swap the user with the computer: the user will pick a number, and the computer will guess until they get it right.
